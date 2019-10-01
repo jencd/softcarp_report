c7384397014d5c661e18e04b3f88af89070ea27a
Jennifer Day <jencd@outlook.com>
2019-10-01 14:30:14 +1000

    Unicorn added

--- sample.R
+++ sample.R
@@ -8,3 +8,7 @@ vector1 <- 1:10
 print("hello")
 
 [all RStudio cheatsheets](https://rstudio.com/resources/cheatsheets/)
+
+![A Unicorn](https://cdn.shopify.com/s/files/1/0257/6087/products/d1b2e81fb85680d23b5c6b752a89c9d3.png?v=1539831273
+
+             
